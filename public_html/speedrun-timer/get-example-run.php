6e3d1021b860b61027a009eabe158dd379bfda51
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-23 12:00:37 +0000

    basic run request

--- public_html/speedrun-timer/get-example-run.php
+++ public_html/speedrun-timer/get-example-run.php
@@ -5,6 +5,7 @@ class RunRequest
   public string $runId;
   public int $historic;
   public string $url;
+  static private string $acceptHeader = "Accept: application/json";
   static private string $baseUrl = "https://splits.io/api/v4/runs/";
 
   function __construct(string $runId, int $historic = 0)
@@ -21,18 +22,19 @@ class RunRequest
       $url = $url . "?historic={$this->historic}";
     $this->url = $url;
   }
-  #CURLOPT_HTTPHEADER	An array of HTTP header fields to set, in the format array:
-  #('Content-type: text/plain', 'Content-length: 100')
+
   function send()
   {
     $ch = curl_init($this->url);
 
     curl_setopt($ch, CURLOPT_HTTPGET, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, (self::$acceptHeader));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     $response = curl_exec($ch);
-    echo json_encode($response);
     curl_close($ch);
+
+    echo json_encode($response);
   }
 }
 
