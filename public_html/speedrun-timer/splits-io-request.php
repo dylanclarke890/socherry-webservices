1cc4786488d32dbb766ba896b1f3f00b8c03389d
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-24 05:11:42 +0000

    built out logic for splits io request

---
+++ public_html/speedrun-timer/splits-io-request.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+namespace SplitsIO;
+
+header('Access-Control-Allow-Origin: *');
+
+enum RequestType:int
+{
+  case RUN = 0;
+  case RUNNER = 1;
+  case GAME = 2;
+  case SEGMENT = 3;
+  case CATEGORY = 4;
+}
+
+class Request
+{
+  private string $endpoint;
+  public int $historic;
+  public string $url;
+  private static string $baseUrl = "https://splits.io/api/v4/";
+  private string $requestId;
+  private array $queryParams;
+
+  function __construct(string $id, RequestType $requestType, mixed $queryData = null)
+  {
+    $this->requestId = $id;
+    switch ($requestType) {
+      case RequestType::RUN:
+        $this->endpoint = "runs/";
+        if (isset($queryData))
+          $this->queryParams = array('historic' => $queryData->historic);
+        break;
+      case RequestType::RUNNER:
+        $this->endpoint = "runners/";
+        break;
+      case RequestType::GAME:
+        $this->endpoint = "games/";
+        break;
+      case RequestType::SEGMENT:
+        $this->endpoint = "segments/";
+        break;
+      case RequestType::CATEGORY:
+        $this->endpoint = "categories/";
+        break;
+      default:
+        break;
+    }
+  }
+
+  public function getRequestUrl(): string
+  {
+    $url = self::$baseUrl . $this->endpoint . $this->requestId;
+    if (isset($this->queryParams) && count($this->queryParams) > 0)
+      $url = $url . http_build_query($this->queryParams);
+    return $url;
+  }
+
+  public function send(): void
+  {
+    $ch = curl_init($this->getRequestUrl());
+
+    curl_setopt($ch, CURLOPT_HTTPGET, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ("Accept: application/json"));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    echo $response;
+  }
+}
+
+$request = new Request("9okq", RequestType::RUN);
+$request->send();
+?>
